271dfc27c1a59f5951fb968669f65480c0d19218
KevinPgn <kevin.pigeon2810@gmail.com>
2023-11-01 16:48:53 +0100

    Add Language switch

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,16 +1,26 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import {changeLanguage} from "../redux/Features/Language";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const {language} = useSelector((state: any) => state.language);
+
   return (
     <div>
-      <h1>TodoList</h1>
-      <nav>
-        <NavLink to="/list">List</NavLink>
-        <NavLink to="/list">List</NavLink>
-        <NavLink to="/list">List</NavLink>
-        <NavLink to="/list">List</NavLink>
-        <NavLink to="/list">List</NavLink>
-      </nav>
+      <div className="button-content">
+        {language === "fr" ? (
+          <button onClick={() => dispatch(changeLanguage("en"))}>
+            Changer en anglais
+          </button>
+        ) : (
+          <button onClick={() => dispatch(changeLanguage("fr"))}>
+            Changer en français
+          </button>
+        )}
+      </div>
+
+      <span>Vous êtes sur un site {language}</span>
       <Outlet />
     </div>
   )
