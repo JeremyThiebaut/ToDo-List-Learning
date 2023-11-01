afae5e5e1e6e355f52f07f3c9d9038c037665e69
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-01 16:47:59 +0100

    Add layout language

--- front-end/src/layouts/HomeLayout.tsx
+++ front-end/src/layouts/HomeLayout.tsx
@@ -1,17 +1,26 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { StateProps } from "@/store/reducer";
+import actionTypes from "@/store/action";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const language = useSelector((state: StateProps) => state.language);
+
+  const toggleLanguage = () => {
+    dispatch({
+      type: actionTypes.CHANGE_LANGUAGE,
+    });
+  };
+
   return (
-    <div>
-      <h1>TodoList</h1>
-      <nav>
-        <NavLink to="/list">List</NavLink>
-        <NavLink to="/list">List</NavLink>
-        <NavLink to="/list">List</NavLink>
-        <NavLink to="/list">List</NavLink>
-        <NavLink to="/list">List</NavLink>
-      </nav>
+    <div className="container">
+      <div className="toggleLang">
+        <button onClick={toggleLanguage}>
+          {language === "en" ? "fr" : "en"}
+        </button>
+      </div>
       <Outlet />
     </div>
-  )
+  );
 }
