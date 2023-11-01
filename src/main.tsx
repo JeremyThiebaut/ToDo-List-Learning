89e654ed8c5d14e39f95dd8441b1555f1566841a
KevinPgn <kevin.pigeon2810@gmail.com>
2023-11-01 16:45:50 +0100

    Add i18N

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import "@/styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
 import {store} from "./redux/Store";
 import {Provider} from "react-redux";
+import I18n from "./i18n.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
+        <I18n />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
