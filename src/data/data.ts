712a284c3fbadcd758001cb2cf51b2afcf62f94b
KevinPgn <kevin.pigeon2810@gmail.com>
2023-11-01 15:40:21 +0100

    Add data

---
+++ src/data/data.ts
@@ -0,0 +1,36 @@
+import { randFirstName, randText } from "@ngneat/falso"
+
+type Todo = {
+    title: string,
+    description: string,
+}
+
+const todoArray: Todo[] = [
+    {
+        title: randFirstName(),
+        description: randText()
+    },
+    {
+        title: randFirstName(),
+        description: randText()
+    },
+    {
+        title: randFirstName(),
+        description: randText()
+    },
+    {
+        title: randFirstName(),
+        description: randText()
+    },
+    {
+        title: randFirstName(),
+        description: randText()
+    },
+    {
+        title: randFirstName(),
+        description: randText()
+    },
+];
+
+
+export default todoArray;
