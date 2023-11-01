d37163a7c0fbf14db080ea2778e537189719228f
KevinPgn <kevin.pigeon2810@gmail.com>
2023-11-01 16:49:31 +0100

    Add language store

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todos from "./Features/TodoTab";
+import language from "./Features/Language";
 
 export const store = configureStore({
     reducer: {
         todos: todos,
+        language: language,
     },
 });
 
