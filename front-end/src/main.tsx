02e9bb91733177e10494d828bac50a7c62867187
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-01 16:47:37 +0100

    Config i18n in app

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -5,12 +5,14 @@ import "@/styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "@/store";
+import I18n from "./i18n.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
+        <I18n />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
