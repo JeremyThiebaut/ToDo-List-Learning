b4b3dd9ed55781ed4b4de72601a18416221b99e0
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-04 21:24:58 +0100

    Change false data

--- back-end/src/query/users.query.ts
+++ back-end/src/query/users.query.ts
@@ -1,9 +1,9 @@
 const QUERY = {
-  SELECT_USERS: `SELECT * FROM users ORDER BY created_at DESC LIMIT $1 OFFSET`,
-  SELECT_USER: `SELECT * FROM users WHERE id = $1`,
-  CREATE_USER: `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`,
-  UPDATE_USER: `UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4`,
-  DELETE_USER: `DELETE FROM users WHERE id = $1`,
+  SELECT_USERS: `SELECT * FROM users ORDER BY created_at DESC LIMIT 100`,
+  SELECT_USER: `SELECT * FROM users WHERE id = ?`,
+  CREATE_USER: `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`,
+  UPDATE_USER: `UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?`,
+  DELETE_USER: `DELETE FROM users WHERE id = ?`,
 };
 
 export default QUERY;
