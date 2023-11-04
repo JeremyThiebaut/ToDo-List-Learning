4440642e2c2d19a597abead725c37e5ce22a1fbf
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-04 13:24:18 +0100

    Structure of BDD

---
+++ back-end/dbinit/init.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS `todosdb`
+
+USE `todosdb`;
+
+DROP TABLE IF EXISTS `todos`;
+
+CREATE TABLE `todos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `users_email_unique` UNIQUE (`email`),
+);
