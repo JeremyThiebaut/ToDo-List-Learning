7f9cbee303491fb614fa9906d820c8c03374f693
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-04 21:01:56 +0100

    Config mysal

--- back-end/src/config/mysql.config.ts
+++ back-end/src/config/mysql.config.ts
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const pool = mysql.createPool({
-  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT),
   host: String(process.env.DB_HOST),
   port: Number(process.env.DB_PORT),
   user: String(process.env.DB_USER),
   password: String(process.env.DB_PASSWORD),
   database: String(process.env.DB_DATABASE),
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT),
 });
 
 export default pool;
