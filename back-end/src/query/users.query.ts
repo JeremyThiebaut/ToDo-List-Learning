0fd56b9726719d16179fbe1f6bde1c6c9d5dd026
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-05 17:12:30 +0100

    Update fetch query

--- back-end/src/query/users.query.ts
+++ back-end/src/query/users.query.ts
@@ -1,6 +1,6 @@
 const QUERY = {
-  SELECT_USERS: `SELECT * FROM users ORDER BY created_at DESC LIMIT 100`,
-  SELECT_USER: `SELECT * FROM users WHERE id = ?`,
+  SELECT_USERS: `SELECT id, username, email FROM users ORDER BY created_at DESC LIMIT 100`,
+  SELECT_USER: `SELECT id, username, email FROM users WHERE id = ?`,
   CREATE_USER: `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`,
   UPDATE_USER: `UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?`,
   DELETE_USER: `DELETE FROM users WHERE id = ?`,
