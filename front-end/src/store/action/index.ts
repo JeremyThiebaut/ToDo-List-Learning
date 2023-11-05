da94f214206f2e376d11d1d1be9e9d738f39cd07
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-05 17:15:33 +0100

    Remove action

--- front-end/src/store/action/index.ts
+++
@@ -1,9 +0,0 @@
-const actionTypes = {
-  ADD_TODO: "ADD_TODO",
-  REMOVE_TODO: "REMOVE_TODO",
-  UPDATE_TODO: "UPDATE_TODO",
-  CHANGE_LANGUAGE: "CHANGE_LANGUAGE",
-  TOGGLE_TODO: "TOGGLE_TODO",
-};
-
-export default actionTypes;
