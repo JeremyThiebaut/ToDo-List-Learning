54d35784cbcd59a99ad45f79e13afd9321ea3c34
Jeremy Thiebaut <jeremy@diggersfactory.com>
2023-11-08 18:08:33 +0100

    Create route of login

--- back-end/src/route/user.route.ts
+++ back-end/src/route/user.route.ts
@@ -5,12 +5,13 @@ import {
   createUser,
   updateUser,
   deleteUser,
+  loginUser,
 } from "../controller/user.controller";
 
 const userRoutes = express.Router();
 
 userRoutes.route("/").get(getUsers).post(createUser);
-
+userRoutes.route("/login").post(loginUser);
 userRoutes.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
 export default userRoutes;
