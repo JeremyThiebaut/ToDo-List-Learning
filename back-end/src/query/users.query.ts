1fb213aa8e62a9c499d5de2a9542ca4e5730f971
Jeremy Thiebaut <jeremy@diggersfactory.com>
2023-11-08 18:08:17 +0100

    Add query to get user

--- back-end/src/query/users.query.ts
+++ back-end/src/query/users.query.ts
@@ -4,6 +4,7 @@ const QUERY = {
   CREATE_USER: `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`,
   UPDATE_USER: `UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?`,
   DELETE_USER: `DELETE FROM users WHERE id = ?`,
+  SELECT_USER_BY_EMAIL_AND_PASSWORD: `SELECT id, username, email FROM users WHERE email = ? AND password = ?`,
 };
 
 export default QUERY;
