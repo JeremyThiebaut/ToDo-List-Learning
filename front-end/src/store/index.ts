c8c2561c54367973917883f7756580d47b0c07dc
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-09 19:32:58 +0100

    Add persist store with localstorage

--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -1,12 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Middleware, configureStore } from "@reduxjs/toolkit";
 import Reducer from "@/store/reducer";
 import UserReducer from "@/store/reducer/userReducer";
+
+const localStorageMiddleware: Middleware = (store) => (next) => (action) => {
+  const result = next(action);
+  localStorage.setItem("applicationState", JSON.stringify(store.getState()));
+  return result;
+};
+
+const persistedState = localStorage.getItem("applicationState")
+  ? JSON.parse(localStorage.getItem("applicationState") || "")
+  : {};
 
 export const store = configureStore({
   reducer: {
     Reducer,
     UserReducer,
   },
+  preloadedState: persistedState,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export default store;
