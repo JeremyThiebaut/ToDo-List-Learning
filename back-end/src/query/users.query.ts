a1c61292dcb8376265379431b15cfce5e42b27e8
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-11-09 19:26:53 +0100

    Change query

--- back-end/src/query/users.query.ts
+++ back-end/src/query/users.query.ts
@@ -1,9 +1,5 @@
 const QUERY = {
-  SELECT_USERS: `SELECT id, username, email FROM users ORDER BY created_at DESC LIMIT 100`,
-  SELECT_USER: `SELECT id, username, email FROM users WHERE id = ?`,
-  CREATE_USER: `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`,
-  UPDATE_USER: `UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?`,
-  DELETE_USER: `DELETE FROM users WHERE id = ?`,
+  INSERT_USER: `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`,
   SELECT_USER_BY_EMAIL_AND_PASSWORD: `SELECT id, username, email FROM users WHERE email = ? AND password = ?`,
 };
 
